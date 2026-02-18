37827af57818e167af15bb9c500e99baea060e93
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-18 17:01:17 +0100

    structure for axios

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,11 @@
 import Header from '../components/header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function ProductsPage() {
+  const [products, setNewProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   return (
     <>
       <h1 className="text-light bg-dark mt-5">Products</h1>;
