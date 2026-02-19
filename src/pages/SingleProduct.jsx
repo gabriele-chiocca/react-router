4412fe95ccbfa3633ce7b4f64a700e38b3ccf3fe
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-19 16:52:02 +0100

    single page product with card

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -5,18 +5,52 @@ import axios from 'axios';
 export default function SingleProduct() {
   const { id } = useParams();
   const [product, SetProduct] = useState();
-  const [loader, setLoader] = useState();
+  const [loader, setLoader] = useState(true);
+  const [error, setError] = useState(null);
 
   const fetchProduct = () => {
-    axios.get(`https://fakestoreapi.com/products/${id}`).then((res) => {
-      console.log(res.data);
-    });
+    axios
+      .get(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        SetProduct(res.data);
+        setLoader(false);
+      })
+      .catch((err) => {
+        setError('Errore nel caricamento del prodotto');
+        setLoader(false);
+      });
   };
+
   useEffect(fetchProduct, []);
+
+  if (loader)
+    return (
+      <p
+        className="bg-warning-subtle p-5
+"
+      >
+        Caricamento...
+      </p>
+    );
+  if (error) return <p className="bg-danger p-5">{error}</p>;
 
   return (
     <>
-      <h1>Sono pagina {id}</h1>
+      <div className="card cardwith">
+        <img
+          src={product.image}
+          className="card-img-top"
+          alt={product.title}
+        ></img>
+        <div className="card-body cardwithbody">
+          <h5 className="card-title">{product.title}</h5>
+          <p className="card-text">{product.price}</p>
+          <a href="#" className="btn btn-primary">
+            Scopri di più
+          </a>
+        </div>
+      </div>
     </>
   );
 }
