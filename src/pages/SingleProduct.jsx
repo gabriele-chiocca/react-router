f91d045b021341ea87c5bff3a815a40f255d75eb
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-19 15:42:38 +0100

    add useParams

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,5 +1,7 @@
 import { useParams } from 'react-router-dom';
 
 export default function SingleProduct() {
+  const { id } = useParams();
+
   return <h1>Sono la singola page di un prodotto</h1>;
 }
