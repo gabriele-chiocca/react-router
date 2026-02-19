1e927b1a903200eb556a6d354652c29ce684cab9
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-02-19 16:44:13 +0100

    Single Product axios and const

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,18 +1,25 @@
+import { Link } from 'react-router-dom';
+
 export default function ProductCard({ product }) {
   return (
     <div className="card cardwith">
-      <img
-        src={product.image}
-        className="card-img-top"
-        alt={product.title}
-      ></img>
-      <div className="card-body cardwithbody">
-        <h5 className="card-title">{product.title}</h5>
-        <p className="card-text">{product.price}</p>
-        <a href="#" className="btn btn-primary">
-          Scopri di più
-        </a>
-      </div>
+      <Link
+        to={'/Products/' + product.id}
+        className="link-body-emphasis text-decoration-none"
+      >
+        <img
+          src={product.image}
+          className="card-img-top"
+          alt={product.title}
+        ></img>
+        <div className="card-body cardwithbody">
+          <h5 className="card-title">{product.title}</h5>
+          <p className="card-text">{product.price}</p>
+          <a href="#" className="btn btn-primary">
+            Scopri di più
+          </a>
+        </div>
+      </Link>
     </div>
   );
 }
--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,6 +1,22 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function SingleProduct() {
   const { id } = useParams();
-  return <h1>Sono la singola page di un prodotto {id}</h1>;
+  const [product, SetProduct] = useState();
+  const [loader, setLoader] = useState();
+
+  const fetchProduct = () => {
+    axios.get(`https://fakestoreapi.com/products/${id}`).then((res) => {
+      console.log(res.data);
+    });
+  };
+  useEffect(fetchProduct, []);
+
+  return (
+    <>
+      <h1>Sono pagina {id}</h1>
+    </>
+  );
 }
